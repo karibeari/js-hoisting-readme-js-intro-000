9a6a5517991b88cd5f80e18a52ec134d69c9d327
Kari Ferenczy <klferenczy@gmail.com>
2019-01-25 21:27:31 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -17,11 +17,6 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  function sayMy() {
     var name = "Kristin";
     console.log(name);
-  }
-  sayMy();
 }
